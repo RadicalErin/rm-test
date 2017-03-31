7a2e58b35d004d444fbcf7bdcf63bfbe67beea60
RadicalErin <erin.eileen.scanlon@gmail.com>
2017-03-31 00:35:59 +0000

    Converting the default apache page to an un-styled base page

---
+++ html/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>
+			Whether
+		</title>
+	</head>
+	<body>
+		<div class="page">
+			<div class="header">
+				<img src="images/cloud-icon.png" alt="logo" class="logo"/>
+				<span class="title">
+					Title
+				</span>
+			</div>
+			<div class="content">
+				Content
+			</div>
+		</div>
+	</body>
+</html>
+
