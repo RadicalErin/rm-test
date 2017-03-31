70a47bb4b96ae591d9420a5ca794f31f69d51112
RadicalErin <erin.eileen.scanlon@gmail.com>
2017-03-31 03:37:44 +0000

    Initial state of page

--- html/index.php
+++ html/index.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<link rel="stylesheet" type="text/css" href="style.css">
-		<title>
-			Whether
-		</title>
-	</head>
-	<body>
-		<div class="page">
-			<div class="header">
-				<div class="logobox">
-					<img src="images/cloud-icon.png" alt="logo" class="logo"/>
-				</div>
-				<div class="title">
-					Whether
-				</div>
-			</div>
-			<div class="content">
-				Content
-			</div>
-		</div>
-	</body>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.3/angular.min.js"></script>
+        <title>
+            Whether
+        </title>
+    </head>
+    <body>
+        <div class="page">
+            <div class="header">
+                <div class="logobox">
+                    <img src="images/cloud-icon.png" alt="logo" class="logo"/>
+                </div>
+                <div class="title">
+                    Whether
+                </div>
+            </div>
+            <div class="content">
+                <div class="loading">
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
 
